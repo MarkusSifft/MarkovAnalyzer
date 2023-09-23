9f364dfb1fd65e462ba49c204f6e6005252b31fd
Markus Sifft <m.sifft@icloud.com>
2023-09-23 11:19:23 +0200

    providing type hints for eager numba compilation

--- src/markovanalyzer/njit_backend.py
+++ src/markovanalyzer/njit_backend.py
@@ -63,9 +63,9 @@ def _fourier_g_prim_njit(nu, eigvecs, eigvals, eigvecs_inv, zero_ind, gpu_0):
     diagonal[~small_indices] = 1 / (-eigvals[~small_indices] - 1j * nu)
     diagonal[zero_ind] = 0
 
-    eigvecs = np.ascontiguousarray(eigvecs)
-    diagonal_matrix = np.ascontiguousarray(np.diag(diagonal))
-    eigvecs_inv = np.ascontiguousarray(eigvecs_inv)
+    #eigvecs = np.ascontiguousarray(eigvecs)
+    #diagonal_matrix = np.ascontiguousarray(np.diag(diagonal))
+    #eigvecs_inv = np.ascontiguousarray(eigvecs_inv)
 
     Fourier_G = eigvecs @ diagonal_matrix @ eigvecs_inv
 
@@ -105,8 +105,8 @@ def _first_matrix_step_njit(rho, omega, a_prim, eigvecs, eigvals, eigvecs_inv, z
     """
 
     G_prim = _fourier_g_prim_njit(omega, eigvecs, eigvals, eigvecs_inv, zero_ind, gpu_0)
-    G_prim = np.ascontiguousarray(G_prim)
-    rho = np.ascontiguousarray(rho)
+    #G_prim = np.ascontiguousarray(G_prim)
+    #rho = np.ascontiguousarray(rho)
     rho_prim = G_prim @ rho
     out = a_prim @ rho_prim
 
