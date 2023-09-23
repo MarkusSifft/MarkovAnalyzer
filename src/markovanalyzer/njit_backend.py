31d5ec7f574dfeddbe1da28bc9b6607f5a48e5d5
Markus Sifft <m.sifft@icloud.com>
2023-09-23 11:11:17 +0200

    providing type hints for eager numba compilation

--- src/markovanalyzer/njit_backend.py
+++ src/markovanalyzer/njit_backend.py
@@ -72,7 +72,7 @@ def _fourier_g_prim_njit(nu, eigvecs, eigvals, eigvecs_inv, zero_ind, gpu_0):
     return Fourier_G
 
 
-@njit(fastmath=True)
+@njit("float64[:,:](float64[:,:], float64, float64[:,:], float64[:,:], float64[:], float64[:,:], int64, int32)", fastmath=True)
 def _first_matrix_step_njit(rho, omega, a_prim, eigvecs, eigvals, eigvecs_inv, zero_ind, gpu_0):
     """
     Calculates first matrix multiplication in Eqs. 110-111 in 10.1103/PhysRevB.98.205143. Used
