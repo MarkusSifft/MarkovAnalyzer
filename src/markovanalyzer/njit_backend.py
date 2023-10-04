0c89161833c813fa1bc1cff2f6b395c004c7d6d3
Markus Sifft <m.sifft@icloud.com>
2023-10-04 20:10:45 +0200

    providing type hints for eager numba compilation

--- src/markovanalyzer/njit_backend.py
+++ src/markovanalyzer/njit_backend.py
@@ -195,7 +195,7 @@ def _third_matrix_step_njit(rho, omega, omega2, omega3, a_prim, eigvecs, eigvals
     return out
 
 
-@njit(fastmath=True)
+@njit("complex128[:](complex128[:], float64, complex128[:,:], complex128[:,:], complex128[:], complex128[:,:], int64, int64)", fastmath=True)
 def _matrix_step_njit(rho, omega, a_prim, eigvecs, eigvals, eigvecs_inv, zero_ind, gpu_0):
     """
     Calculates one matrix multiplication in Eqs. 109 in 10.1103/PhysRevB.98.205143. Used
