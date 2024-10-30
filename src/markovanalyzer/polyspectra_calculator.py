a107f405865b21736cac5e8022ffe50313714390
Markus Sifft <m.sifft@icloud.com>
2024-10-30 14:54:33 +0100

    initial distribution for parameteric bootstrapping is now normalized to 1

--- src/markovanalyzer/polyspectra_calculator.py
+++ src/markovanalyzer/polyspectra_calculator.py
@@ -301,6 +301,7 @@ def rates_to_matrix(rates):
         states.add(to_state)
 
     states = sorted(list(states))
+    print('unique states', states)
     n = len(states)
 
     # Initialize a zero matrix
@@ -309,6 +310,7 @@ def rates_to_matrix(rates):
     # Fill the transition rates
     for key, rate in rates.items():
         from_state, to_state = key.split("->")
+        print('from_state', from_state, ' to_state', to_state)
         i, j = states.index(from_state), states.index(to_state)
         matrix[i, j] = rate
 
